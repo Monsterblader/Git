48d70cd7e9004b29c7f771aeea7bec091c072178
Albert Lee <monsterblader@gmail.com>
2012-11-27 23:50:03 -0800

    Trash can images added.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+var i=0;
+function processForm() {
+
+	i++;
+	var listItem = document.myCheckListForm.checklistItem.value;
+	var listed = '<div id="item' + i + '"><input type="checkbox" onclick="crossedOut(\'item' + i + '\')" />' + listItem + '</div>';
+
+	document.getElementById("checkList").innerHTML += listed;
+}
+
+function crossedOut(item) {
+	document.getElementById(item).className = "checked-off";
+}
 function sayHello() {
 document.getElementById("result").innerHTML = 'What an amazing guy!';
 }
